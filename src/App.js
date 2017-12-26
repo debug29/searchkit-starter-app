bcfe0872da4944c3b2fbcbe0fcdb0889635d1353
Florian <f.m.coulon@gmail.com>
2017-12-26 10:41:53 +0100

    Test remove picture

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const MovieHitsGridItem = (props)=> {
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <a href={url} target="_blank">
-        <img data-qa="poster" alt="presentation" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
         <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></div>
       </a>
     </div>
