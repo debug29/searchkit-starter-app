48134324d39793ccdfae074272b8545aec0a5886
Florian <f.m.coulon@gmail.com>
2017-12-26 12:06:57 +0100

    default ?

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import './index.css'
 const host = "http://lgc-sandbox-dev:9200/console"
 const searchkit = new SearchkitManager(host)
 
+searchkit.addDefaultQuery(query => {
+  return query.addQuery(QueryString("type:flux", null);
+});
+
 class App extends Component {
   render() {
     return (
