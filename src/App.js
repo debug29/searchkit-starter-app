651cbbb4707672fd532e92a85d2daf02a03049c8
Florian <f.m.coulon@gmail.com>
2017-12-26 11:15:04 +0100

    result ?

--- src/App.js
+++ src/App.js
@@ -10,19 +10,8 @@ import { SearchkitManager,SearchkitProvider,
   ActionBar, ActionBarRow, SideBar } from 'searchkit'
 import './index.css'
 
-const host = "http://lgc-sandbox-dev:9200/console/"
+const host = "http://lgc-sandbox-dev:9200/console"
 const searchkit = new SearchkitManager(host)
-
-const MovieHitsGridItem = (props)=> {
-  const {bemBlocks, result} = props
-  const source = extend({}, result._source, result.highlight)
-  return (
-    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.id_flu}}></div>
-      
-    </div>
-  )
-}
 
 class App extends Component {
   render() {
@@ -36,34 +25,18 @@ class App extends Component {
 
         <LayoutBody>
 
-          <SideBar>
-            <HierarchicalMenuFilter fields={["type"]} title="Categories" id="categories"/>
-          </SideBar>
           <LayoutResults>
             <ActionBar>
-
               <ActionBarRow>
                 <HitsStats translations={{
                   "hitstats.results_found":"{hitCount} results found"
                 }}/>
-                <ViewSwitcherToggle/>
               </ActionBarRow>
 
               <ActionBarRow>
-                <GroupedSelectedFilters/>
                 <ResetFilters/>
               </ActionBarRow>
-
             </ActionBar>
-            <ViewSwitcherHits
-                hitsPerPage={12}
-                hitComponents={[
-                  {key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
-                ]}
-                scrollTo="body"
-            />
-            <NoHits/>
-            <Pagination showNumbers={true}/>
           </LayoutResults>
 
           </LayoutBody>
